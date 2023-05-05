dc53eb8fb42067d0b0eb2815928777cd315bfc92
Pedro H. Alves <78762616+LeonKene-hub@users.noreply.github.com>
2023-05-05 09:19:27 -0300

    Add files via upload

---
+++ TryCatch/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine($"Bem Vindo ao programa do Edu");
+
+int idade = -1;
+
+do
+{
+
+    try
+    {
+        Console.WriteLine("Digite sua idade:");
+        idade = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"Idade: {idade}");
+    }
+    catch (System.Exception)
+    {
+
+        Console.WriteLine($"É necessário digitar números!");
+
+    }
+
+} while (idade < 0);
